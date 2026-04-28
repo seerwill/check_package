c2567cd5b5bce18d8bcb6dd6118113d9a10d0051
Will Hart <will@seermedical.com>
2026-04-29 09:26:50 +1000

    Refactor to use CLI tools

---
+++ src/checker.rs
@@ -0,0 +1,57 @@
+use anyhow::anyhow;
+use xshell::{Shell, cmd};
+
+pub trait Checker {
+    fn check(&self, shell: &Shell, package: &String, version: &String) -> anyhow::Result<bool>;
+}
+
+#[derive(Default)]
+pub struct JsChecker;
+
+impl Checker for JsChecker {
+    fn check(&self, shell: &Shell, package: &String, version: &String) -> anyhow::Result<bool> {
+        let command = cmd!(shell, "yarn why {package}");
+
+        match command.output() {
+            Ok(result) => {
+                let stdout = String::from_utf8(result.stdout)?;
+                let stderr = String::from_utf8(result.stderr)?;
+
+                if stdout.contains("Found") && stdout.contains(version) {
+                    Ok(true)
+                } else if stderr.contains("couldn't find a match") {
+                    Ok(false)
+                } else {
+                    Err(anyhow!(stdout))
+                }
+            }
+            Err(e) => Err(anyhow!(e)),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct CargoChecker {
+    tree: String,
+}
+
+impl CargoChecker {
+    pub fn new(folder_path: &String) -> anyhow::Result<Self> {
+        let sh = Shell::new()?;
+        sh.change_dir(folder_path);
+        let tree = cmd!(sh, "cargo tree").read()?;
+
+        Ok(Self { tree })
+    }
+}
+
+impl Checker for CargoChecker {
+    fn check(&self, _shell: &Shell, package: &String, version: &String) -> anyhow::Result<bool> {
+        Ok(self
+            .tree
+            .lines()
+            .filter(|l| l.contains(&format!("{package} v")) && l.contains(version))
+            .collect::<Vec<_>>()
+            .is_empty())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,72 +1,113 @@
-use anyhow::bail;
+use anyhow::{Context, anyhow};
+use serde::{Deserialize, Serialize};
+use xshell::Shell;
 
-const DATA_PATH: &'static str = "data.txt";
-const REPO_PATH: &'static str = "repos.txt";
+use crate::checker::{CargoChecker, Checker, JsChecker};
 
-/// Takes a line from the data path file and extracts the package
-fn parse_line(line: &str) -> anyhow::Result<String> {
-    match line.split(',').next() {
-        Some(v) => Ok(v.to_string()),
-        None => bail!("Invalid line - {line}"),
+mod checker;
+
+const DATA_PATH: &'static str = "data.json";
+const REPO_PATH: &'static str = "repos.json";
+
+#[derive(Clone, Copy, Debug, Serialize, Deserialize)]
+pub enum Language {
+    JS,
+    Rust,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct Repo {
+    pub folder_path: String,
+    pub language: Language,
+    #[serde(skip)]
+    packages: Vec<Package>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct RepoList {
+    repos: Vec<Repo>,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct Package {
+    pub name: String,
+    pub versions: Vec<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct PackageList {
+    packages: Vec<Package>,
+}
+
+impl Repo {
+    pub fn check(&self) -> anyhow::Result<Vec<String>> {
+        let sh = Shell::new()?;
+        sh.change_dir(&self.folder_path);
+
+        let checker: Box<dyn Checker> = match self.language {
+            Language::JS => Box::new(JsChecker::default()),
+            Language::Rust => Box::new(CargoChecker::new(&self.folder_path)?),
+        };
+
+        Ok(self
+            .packages
+            .iter()
+            .flat_map(|pkg| {
+                let package = &pkg.name;
+
+                pkg.versions
+                    .iter()
+                    .filter_map(|version| {
+                        let result = checker
+                            .check(&sh, package, version)
+                            .context("Running repo Check")
+                            .expect("Error running query");
+
+                        if result {
+                            Some(format!("{package}@{version}"))
+                        } else {
+                            None
+                        }
+                    })
+                    .collect::<Vec<_>>()
+            })
+            .collect())
     }
 }
 
-/// Checks the lock file at the given path and prints out whether or not
-/// it contains the vulnerable packages specified in the `packages` variable
-fn is_repository_vulnerable(lockfile: &str, packages: &[String]) -> anyhow::Result<bool> {
-    let mut vulnerable = false;
+fn main() -> anyhow::Result<()> {
+    let repo_list = serde_json::from_str::<RepoList>(
+        &std::fs::read_to_string(REPO_PATH).context("Find repo file")?,
+    )?;
+    let package_list = serde_json::from_str::<PackageList>(
+        &std::fs::read_to_string(DATA_PATH).context("Find data file")?,
+    )?;
 
-    for line in lockfile.lines() {
-        for package in packages {
-            if line.contains(package) {
-                println!(" --> potential vulnerability - {line}");
-                vulnerable = true;
+    let mut found_bad = false;
+
+    for repo in repo_list.repos {
+        println!("Checking {}", &repo.folder_path);
+
+        // lots of cloning but who cares its a throwaway CLI
+        let mut r = repo.clone();
+        r.packages = package_list.packages.clone();
+
+        let vulnerable_packages = r.check()?;
+        if vulnerable_packages.is_empty() {
+            println!("    no vulnerable package versions found");
+        } else {
+            found_bad = true;
+
+            println!("    VULNERABLE PACKAGES FOUND");
+            for msg in vulnerable_packages {
+                println!("    {}", msg);
             }
         }
     }
 
-    Ok(vulnerable)
-}
-
-fn main() -> anyhow::Result<()> {
-    let packages = std::fs::read_to_string(DATA_PATH)?
-        .lines()
-        .map(parse_line)
-        .collect::<Result<Vec<String>, anyhow::Error>>()?;
-
-    for repo in std::fs::read_to_string(REPO_PATH)?.lines() {
-        println!("Checking {repo}");
-        let lockfile = std::fs::read_to_string(repo)?;
-
-        if is_repository_vulnerable(&lockfile, &packages)? {
-            println!(" --> REPO IS POTENTIALLY VULNERABLE");
-        } else {
-            println!(" --> Repo doesn't appear to be vulnerable");
-        }
-    }
-
-    Ok(())
-}
-
-#[cfg(test)]
-mod test {
-    use crate::is_repository_vulnerable;
-
-    #[test]
-    fn test_is_vulnerable() -> anyhow::Result<()> {
-        assert!(is_repository_vulnerable(
-            "knex\nknex-migrate\nis-ansi",
-            &["knex".into()]
-        )?);
-        assert!(!is_repository_vulnerable(
-            "knex\nknex-migrate\nis-ansi",
-            &["knexo".into()]
-        )?);
-        assert!(!is_repository_vulnerable(
-            "knex\nknex-migrate\nis-ansi",
-            &["my-test-repo".into()]
-        )?);
-
+    if !found_bad {
         Ok(())
+    } else {
+        Err(anyhow!("Found bad packages"))
     }
 }
